1be580e33a234633c35359f5cebeffcc3e986f72
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-11-28 22:02:40 +0100

    replace test_get_sentence by test_get_all_sentences and rename the unique test inside to test_get_all_sentences to be more precise according to what the called API actually does

--- tests/test_get_all_sentences.rs
+++ tests/test_get_all_sentences.rs
@@ -3,7 +3,7 @@ extern crate reqwest;
 use reqwest::StatusCode;
 
 #[test]
-fn test_get_sentence_returns_200() {
+fn test_get_all_sentences_returns_200() {
 
     let response = reqwest::get("http://localhost:8000/sentences").unwrap();
 
