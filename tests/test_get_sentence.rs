eff3fb0d710648a84ebf4bd8d9056e7c21f447db
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-12-02 22:20:22 +0100

    add a test that checks that requesting GET sentence with a non existing sentence uuid simply returns not found status code

--- tests/test_get_sentence.rs
+++ tests/test_get_sentence.rs
@@ -15,6 +15,8 @@ pub struct Sentence {
     pub structure: Option<String>,
 }
 
+static SERVICE_URL: &'static str = "http://localhost:8000";
+
 #[test]
 fn test_get_sentence_if_exists_returns_200() {
 
@@ -32,7 +34,8 @@ fn test_get_sentence_if_exists_returns_200() {
     );
 
     let url = format!(
-        "http://localhost:8000/sentences/{}",
+        "{}/sentences/{}",
+        SERVICE_URL,
         sentence_uuid.to_string(),
     );
     let mut response = reqwest::get(&url).unwrap();
@@ -54,5 +57,26 @@ fn test_get_sentence_if_exists_returns_200() {
         sentence.iso639_3,
         sentence_iso639_3,
         "Unexpected sentence language.",
+    );
+}
+
+#[test]
+fn test_get_sentence_if_does_not_exist_returns_404() {
+
+    let connection = db::get_connection();
+    db::clear(&connection);
+
+    let sentence_uuid = uuid::Uuid::new_v4();
+
+    let url = format!(
+        "{}/sentences/{}",
+        SERVICE_URL,
+        sentence_uuid.to_string(),
+    );
+    let response = reqwest::get(&url).unwrap();
+
+    assert_eq!(
+        response.status(),
+        StatusCode::NotFound,
     );
 }
