632d9e449f0a13548acc9e217db7d5d34cc4f4ff
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-12-02 22:43:51 +0100

    define a test that checks that POST sentence returns a conflict status code if the sent uuid already exists in sentences table

--- tests/test_create_sentence.rs
+++ tests/test_create_sentence.rs
@@ -1,4 +1,5 @@
 extern crate reqwest;
+extern crate uuid;
 
 use std::collections::HashMap;
 
@@ -25,5 +26,38 @@ fn test_post_sentence_returns_200() {
     assert_eq!(
         response.status(),
         StatusCode::Created,
+    );
+}
+
+#[test]
+fn test_post_sentence_with_used_uuid_returns_409() {
+
+    let connection = db::get_connection();
+    db::clear(&connection);
+
+    let sentence_uuid = uuid::Uuid::new_v4();
+    let sentence_text = "This is one sentence";
+    let sentence_iso639_3 = "eng";
+    db::insert_sentence(
+        &connection,
+        &sentence_uuid,
+        &sentence_text,
+        &sentence_iso639_3,
+    );
+
+    let mut json = HashMap::new();
+    json.insert("id", sentence_uuid.to_string());
+    json.insert("text", sentence_text.to_string());
+    json.insert("iso639_3", sentence_iso639_3.to_string());
+
+    let client = reqwest::Client::new();
+    let response = client.post("http://localhost:8000/sentences")
+        .json(&json)
+        .send()
+        .unwrap();
+
+    assert_eq!(
+        response.status(),
+        StatusCode::Conflict,
     );
 }
