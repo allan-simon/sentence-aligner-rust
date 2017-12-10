aeccb6c7013b8b66cd4d6e297663cbe54267254c
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-12-10 19:31:38 +0100

    define interface test that checks put sentence language succeeds (#20)

--- tests/test_put_sentence.rs
+++ tests/test_put_sentence.rs
@@ -79,3 +79,47 @@ fn test_put_sentence_text_that_does_not_exist_returns_403() {
         StatusCode::NotFound,
     );
 }
+
+#[test]
+fn test_put_sentence_language_returns_200() {
+
+    let connection = db::get_connection();
+    db::clear(&connection);
+
+    let sentence_uuid = uuid::Uuid::new_v4();
+    let sentence_text = "This is one sentence";
+    let sentence_iso639_3 = "eng";
+    db::insert_sentence(
+        &connection,
+        &sentence_uuid,
+        &sentence_text,
+        &sentence_iso639_3,
+    );
+
+    let client = reqwest::Client::new();
+
+    let url = format!(
+        "{}/sentences/{}/language",
+        tests_commons::SERVICE_URL,
+        sentence_uuid,
+    );
+    let modified_language = "fra";
+    let response = client.put(&url)
+        .body(modified_language)
+        .header(ContentType::plaintext())
+        .send()
+        .unwrap();
+
+    assert_eq!(
+        response.status(),
+        StatusCode::NoContent,
+    );
+
+    assert_eq!(
+        db::get_language(
+            &connection,
+            &sentence_uuid,
+        ),
+        modified_language,
+    );
+}
