e8bd2fb017948bb2ce7b697cd435524f603685b0
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-12-30 22:35:58 +0100

    create one test to check that posting a sentence with a structure is possible

--- tests/test_create_sentence.rs
+++ tests/test_create_sentence.rs
@@ -83,6 +83,45 @@ fn test_post_sentence_without_structure_returns_200() {
 }
 
 #[test]
+fn test_post_sentence_with_structure_returns_200() {
+
+    let connection = db::get_connection();
+    db::clear(&connection);
+
+    let mut json = HashMap::new();
+    let sentence_uuid = uuid::Uuid::new_v4();
+    let sentence_structure = "<sentence><subject>This</subject> <verb>is</verb> <complement>a</complement> <complement>sentence</complement></sentence>";
+    json.insert("id", sentence_uuid.to_string());
+    json.insert("text", "This is a sentence.".to_string());
+    json.insert("iso639_3", "eng".to_string());
+    json.insert("structure", sentence_structure.to_string());
+
+    let client = reqwest::Client::new();
+
+    let url = format!(
+        "{}/sentences",
+        tests_commons::SERVICE_URL,
+    );
+    let response = client.post(&url)
+        .json(&json)
+        .send()
+        .unwrap();
+
+    assert_eq!(
+        response.status(),
+        StatusCode::Created,
+    );
+
+    assert_eq!(
+        db::get_structure(
+            &connection,
+            &sentence_uuid,
+        ),
+        sentence_structure.to_string()
+    );
+}
+
+#[test]
 fn test_post_sentence_with_used_uuid_returns_409() {
 
     let connection = db::get_connection();
