408795c56fea29b1fbd4d3be5cdeae54091f0c62
Jean LELIEVRE <lelievrejean82@gmail.com>
2018-03-20 22:29:41 +0100

    Use tests helpers for tests post language (#84)
    * upgrade interface tests helpers lib to have conflict, server error, bad request status codes
    
    * use the rust interface tests helpers for the post language tests

--- tests/handlers.rs
+++ tests/handlers.rs
@@ -23,9 +23,25 @@ impl HasBaseUrl for Client {
     }
 }
 
+pub trait LanguageHandler {
+
+    fn post_language(&self, iso_639_3: &str) -> Response;
+}
+
 pub trait SentenceHandler {
 
     fn post_sentence(&self, json: &HashMap<&str, &str>) -> Response;
+}
+
+impl LanguageHandler for Client {
+
+    fn post_language(&self, iso_639_3: &str) -> Response {
+
+        self.post_body(
+            &format!("{}/languages", self.get_base_url()),
+            iso_639_3,
+        )
+    }
 }
 
 impl SentenceHandler for Client {
